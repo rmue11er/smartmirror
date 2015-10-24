26e8f659a540ea5e52a39188d92c8eca457909f8
René Uchiha <rene.uchiha@mail.com>
2015-10-25 00:14:28 +0200

    edited time.js icons

--- modules/time/time.js
+++ modules/time/time.js
@@ -1,27 +1,15 @@
 function getTimeIcon(){
 	var icon = null;
-	var time = moment().format("HH");
+	var hour = moment().hours();
 	
-	if(moment().endOf('day').fromNow() < 6){
-		icon = '<i class="wi wi-day-haze"></i>';
-	}
 	
-	if(moment().endOf('day').fromNow() < 4){
-		icon = '<i class="wi wi-night-clear"></i>';
-	}
+	if(hour >= 0) icon = '<i class="wi wi-night-clear"></i>';
+	if(hour > 2) icon = '<i class="wi wi-moonset"></i>';
+	if(hour > 4) icon = '<i class="wi wi-sunrise"></i>';
+	if(hour > 11) icon = '<i class="wi wi-day-cloudy-high"></i>';  //@TODO check current weather
+	if(hour > 18) icon = '<i class="wi wi-day-haze"></i>';
+	if(hour > 20) icon = '<i class="wi wi-moonrise"></i>'; 
+	if(hour > 22) icon = '<i class="wi wi-night-clear"></i>';
 	
-	if(moment().endOf('day').fromNow() < 24){
-		icon = '<i class="wi wi-sunrise"></i>';
-	}
-	
-	if(moment().startOf('day').fromNow() > 0){
-		icon = '<i class="wi wi-sunrise"></i>';
-	}
-	
-	if(moment().endOf('day').fromNow() > 12){
-		icon = '<i class="wi wi-day-sun"></i>';
-	}
-	
-	alert(moment().endOf('day').fromNow());
 	return icon;
 }
