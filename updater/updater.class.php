883e2c7202a2a8825e75bafae012fa83b4915572
René Uchiha <rene.uchiha@mail.com>
2015-10-29 21:13:01 +0100

    fixed loadFile

--- updater/updater.class.php
+++ updater/updater.class.php
@@ -152,14 +152,7 @@ class Updater {
 	}
 	
 	function loadFile($url) {
-		$ch = curl_init();
-	
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url);
-	
-		$data = curl_exec($ch);
-		curl_close($ch);
+		$data = file_get_contents($url);
 	
 		return $data;
 	}
